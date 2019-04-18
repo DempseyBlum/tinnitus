15586fc2506d2461b1c6107e58eae480c6540e42
Dempsey Blum <dempsey@curiousit.com>
2019-04-18 14:18:24 +1000

    changing homepage template to match preview template

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -36,9 +36,24 @@ const HomePage = ({ data }) => {
   return (
     <Layout>
       <HomePageTemplate
-        main = {frontmatter.sidebar}
-        sidebar = {frontmatter.sidebar}
-        donate = {frontmatter.donate}
+        main = {{
+          heading: entry.getIn(['data', 'main', 'heading']),
+          body: entry.getIn(['data', 'main', 'body']),
+          heading2: entry.getIn(['data', 'main', 'heading2']),
+          body2: entry.getIn(['data', 'main', 'body2']),
+          heading3: entry.getIn(['data', 'main', 'heading3']),
+          body3: entry.getIn(['data', 'main', 'body3'])
+        }}
+        sidebar = {{
+          heading4: entry.getIn(['data', 'sidebar', 'heading4']),
+          body4: entry.getIn(['data', 'sidebar', 'body4']),
+          heading5: entry.getIn(['data', 'sidebar', 'heading5']),
+          body5: entry.getIn(['data', 'sidebar', 'body5'])
+        }}
+        donate = {{
+          heading6: entry.getIn(['data', 'donate', 'heading6']),
+          body6: entry.getIn(['data', 'donate', 'body6'])
+        }}
       />
     </Layout>
   )
