af01df8d471672ffdb01df7f340be6fc89f6dca8
Dempsey Blum <dempsey@curiousit.com>
2019-04-18 17:56:18 +1000

    updating preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,3 +3,4 @@ import CMS from 'netlify-cms'
 import HomePagePreview from './preview-templates/HomePagePreview'
 
 CMS.registerPreviewTemplate('home', HomePagePreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview)
